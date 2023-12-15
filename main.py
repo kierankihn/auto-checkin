a274b3b7c00b47fbfb6457b6f06219906b09577f
Kieran Kihn <114803508+kierankihn@users.noreply.github.com>
2023-12-15 13:21:04 +0800

    feat: updated checkin

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def checkin():
     load_dotenv()
 
     headers={'Cookie': os.getenv("LUOGU_COOKIE"),
-         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'}
+             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'}
 
     response=requests.get('https://www.luogu.com.cn/index/ajax_punch', headers=headers)
 
