ad47fab1da19df0176aa5cfd26589698196df16f
Kieran Kihn <114803508+kierankihn@users.noreply.github.com>
2023-12-15 13:31:17 +0800

    feat: updated .env error

--- main.py
+++ main.py
@@ -5,9 +5,8 @@ import schedule
 import requests
 from dotenv import load_dotenv
 
-def checkin():
 
-    load_dotenv()
+def checkin():
 
     headers={'Cookie': os.getenv("LUOGU_COOKIE"),
              'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'}
@@ -17,6 +16,13 @@ def checkin():
     res=json.loads(response.content.decode("unicode_escape"))
 
     print(res.get("message"))
+
+
+load_dotenv()
+
+if (os.getenv("LUOGU_COOKIE") == None):
+    print("Cannot read LUOGU_COOKIE in .env file, please make sure that you have already configured envirment variables.")
+
 
 schedule.every().days.at("10:00", "Asia/Shanghai").do(checkin)
 
