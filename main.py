7c4f96e64443f84e28df68e8926c437587a37ba1
Kieran Kihn <114803508+kierankihn@users.noreply.github.com>
2023-12-17 21:17:43 +0800

    feat: use config.json instead of .env

--- main.py
+++ main.py
@@ -1,33 +1,42 @@
-import os
 import time
 import json
 import schedule
 import requests
-from dotenv import load_dotenv
+
+
+def getConfig():
+    with open('config.json', 'r') as configFile:
+        global config
+        config = json.load(configFile)
 
 
 def checkin():
+    for luoguCookie in config.get('token'):
 
-    headers={'Cookie': os.getenv("LUOGU_COOKIE"),
+        headers={'Cookie': '__client_id=' + luoguCookie.get('__client_id') + '; _uid=' + str(luoguCookie.get('_uid')) + ';',
              'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'}
 
-    response=requests.get('https://www.luogu.com.cn/index/ajax_punch', headers=headers)
+        response=requests.get('https://www.luogu.com.cn/index/ajax_punch', headers=headers)
 
-    res=json.loads(response.content)
+        res=json.loads(response.content)
 
-    if (response.status_code == 200):
-        print(res.get("more"))
-    if (response.status_code == 201):
-        print(res.get("message"))
+        print('正在打卡：uid = ' + str(luoguCookie.get('_uid')))
+
+        if (response.status_code != 200):
+            print('打卡失败，错误信息：')
+            print(response)
+        else:
+            if (res.get('code') == 200):
+                print('打卡成功！')
+            if (res.get('code') == 201):
+                print('今天已经打过卡了')
 
 
-load_dotenv()
+getConfig()
 
-if (os.getenv("LUOGU_COOKIE") == None):
-    print("Cannot read LUOGU_COOKIE in .env file, please make sure that you have already configured envirment variables.")
+checkin()
 
-
-schedule.every().days.at("10:00", "Asia/Shanghai").do(checkin)
+# schedule.every().days.at('10:00', 'Asia/Shanghai').do(checkin)
 
 while True:
     schedule.run_pending()
